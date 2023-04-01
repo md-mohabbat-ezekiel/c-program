cbf94690a3528c0aee0d960b600d2ba5fc943770
Your Name <mdmohabbatezekiel@gmail.com>
2023-04-01 14:43:42 +0600

    setup

---
+++ c/odd/even.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the value of n");
+    scanf("%d",&n);
+
+    if(n%2==0)
+    {
+        printf(" Even number");
+    }
+
+    else
+    {
+
+        printf("odd number");
+    }
+
+return 0;
+}
---
+++ c/tempCodeRunnerFile.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,sum &n;
+printf("Enter the number",n);
+scanf("%d",&a,&b,&c);
+sum=a+b+c;
+
+
+
+
+
+
+    return 0;
+}
